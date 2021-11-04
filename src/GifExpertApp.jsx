475ab5930bffe51b8cc4ce7bd866ea62b025f8fb
sergio-gonzalez-sainz <s3rgio.sainz@gmail.com>
2021-11-03 23:57:03 -0600

    feat: Add a input box for category fuction

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,19 +1,21 @@
 import React, {useState} from 'react'
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
     
-    // const categories =['One Punch', 'Samurai X', 'Dragon Ball'];
     const [categories, setCategories] = useState(['One Punch','Samurai','Dragon Ball']);
 
-    const handleAdd = () => {
-        setCategories (cats => ['HunterXHunter',...categories]);
-    }
+    // const handleAdd = () => {
+    //     setCategories (cats => ['HunterXHunter',...categories]);
+    // }
+
+
 
     return (
         <div>
             <h1>GifExpertApp</h1>
+            <AddCategory/>
             <hr/>
-            <button  onClick = { handleAdd } >Agregar</button>
             {
                 <ol>
                     {
